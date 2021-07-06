ac3d65eeff28864a615e374eea2bf5cbdcfd133c
kacper8989 <kacper.wybierala1@gmail.com>
2021-07-06 15:45:07 +0200

    UpdateDatesRandom test

--- Toci.Berserk.Tests/BllMl/SuspectTest.cs
+++ Toci.Berserk.Tests/BllMl/SuspectTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.Berserk.Bll;
 using Toci.Berserk.Bll.Ml;
 using Toci.Berserk.Database.Persistence.Models;
 
@@ -9,10 +11,29 @@ namespace Toci.Berserk.Tests.BllMl
     public class SuspectTest
     {
         [TestMethod]
-        public void SuspectOrderTest()
+        public void SuspectLastOrderTest()
         {
             SuspectOrderLogic suspectOrderLogic = new SuspectOrderLogic();
             Order lastOrder = suspectOrderLogic.LastAccomplishedOrderDate();
+        }
+
+        [TestMethod]
+        public void UpdateDatesRandom()
+        {
+            LogicBase<Chemistrypop> chemistry = new LogicBase<Chemistrypop>();
+            Random r = new Random();
+
+            for (int j = 0, z=0; j < 1000; j+=50, z++)
+            {
+                IQueryable <Chemistrypop> chemistrypops = chemistry.Select(model => model.Id > j && model.Id < j+50);
+
+                foreach (Chemistrypop el in chemistrypops)
+                {
+                    el.Idproducts = r.Next(1, 20);
+                    el.Date = DateTime.Now.AddDays(-(z));
+                    chemistry.Update(el);
+                }
+            }
         }
     }
 }
