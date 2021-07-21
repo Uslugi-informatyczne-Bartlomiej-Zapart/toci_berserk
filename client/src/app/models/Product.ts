71990c7a9b9cd677bb21443b2643b295e5483a63
Tomek <tomek1989gw@wppl>
2021-07-22 01:23:54 +0200

    Add/Remove products from company

---
+++ client/src/app/models/Product.ts
@@ -0,0 +1,9 @@
+
+export interface Product {
+    productId: number
+    productName: string
+    productCode: number
+    quantity: number
+    productPriceBasedOnCurrentCompany: number
+    added?: boolean
+}
--- client/src/app/order/new-order-by-company/new-order-by-company.component.ts
+++ client/src/app/order/new-order-by-company/new-order-by-company.component.ts
@@ -1,8 +1,6 @@
-import { componentFactoryName } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { config } from 'rxjs';
-import { Company, MyGroupCompanies } from 'src/app/models/Company';
+import { Product } from 'src/app/models/Product';
 import { ProductCompanySearchService } from 'src/app/services/product-company-search.service';
 
 @Component({
@@ -12,25 +10,37 @@ import { ProductCompanySearchService } from 'src/app/services/product-company-se
 })
 export class NewOrderByCompanyComponent implements OnInit {
 
-  deliveryName: string = ""
+  deliveryInputValue: string = ""
 
   allDeliveryCompanies: any = []
 
   foundedCompanies: any = []
 
-  foundedProducts: any = []
+  foundedProducts: Product[] = []
+
+  addedProducts: Product[] = []
+
+  nowFoundedProductsCompanyName: any = ""
 
   constructor(private prodCompService: ProductCompanySearchService) { }
 
   ngOnInit(): void {
     this.prodCompService.getDeliveryCompanies().subscribe((response: any) => {
-      console.log(response)
       this.allDeliveryCompanies = response
     })
   }
 
   addProduct(idx: number) {
 
+    let prod = Object.assign({}, this.foundedProducts[idx]);
+
+    this.addedProducts.push(prod)
+
+    this.foundedProducts[idx].added = true
+  }
+
+  addProducts() {
+    console.log("Add all products to db")
   }
 
   searchDeliveriesByCharacters(form: NgForm) {
@@ -48,19 +58,51 @@ export class NewOrderByCompanyComponent implements OnInit {
     for (const [key, value] of Object.entries(this.allDeliveryCompanies)) {
       if(value == this.foundedCompanies[idx]) {
         foundedId = key
+        this.nowFoundedProductsCompanyName = value
         break
       }
     }
 
     this.prodCompService.findProductsForCompany(foundedId).subscribe( (response: any) => {
-      console.log(response)
+
       this.foundedProducts = response
-      this.deliveryName = ""
+      this.deliveryInputValue = ""
       this.foundedCompanies = []
     })
 
   }
 
+  deleteProduct(idx: number) {
+    let name = this.addedProducts[idx].productName
 
+    this.setNotAdded(name)
+
+    this.addedProducts.splice(idx, 1)
+  }
+
+  setNotAdded(name: string) {
+
+    for (const [key, value] of Object.entries(this.foundedProducts)) {
+
+      if(value.productName == name) {
+        value.added = false
+        return
+      }
+    }
+  }
+
+  resetView() {
+    this.foundedProducts = []
+    this.addedProducts = []
+    this.nowFoundedProductsCompanyName = ""
+  }
+
+  quantityUp(idx: number) {
+    this.addedProducts[idx].quantity++
+  }
+
+  quantityDown(idx: number) {
+    this.addedProducts[idx].quantity--
+  }
 
 }
